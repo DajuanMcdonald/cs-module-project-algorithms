65cfacb8f74dd587f6f760b34496e0159ef85083
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-07-14 19:39:55 -0400

    single_number passed

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,15 +1,23 @@
 '''
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
-'''
-def single_number(arr):
-    # Your code here
 
-    pass
+input: array
+output: integer [9]
+
+'''
+
+
+def single_number(arr):
+	# loop list
+	number = 0
+	for i in arr:
+		number ^= i
+	return number
 
 
 if __name__ == '__main__':
-    # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+	# Use the main function to test your implementation
+	arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
-    print(f"The odd-number-out is {single_number(arr)}")
+	print(f"The odd-number-out is {single_number(arr)}")
